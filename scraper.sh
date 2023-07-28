184466cc5ffe6a85c1545ff25456ec741e20e91c
bnonni <bryan@nonni.io>
2023-07-27 20:17:18 -0400

    init push

---
+++ scraper.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+wget \
+    --recursive \
+    --no-clobber \
+    --page-requisites \
+    --html-extension \
+    --convert-links \
+    --restrict-file-names=windows \
+    --domains $1 \
+    --no-parent https://$1
